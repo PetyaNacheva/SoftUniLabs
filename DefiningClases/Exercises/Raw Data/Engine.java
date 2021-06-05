7d9c981b5b023c1992a705bb7185ed5d5811c836
Petya Nacheva <79047302+PetyaNacheva@users.noreply.github.com>
2021-06-05 16:53:22 +0300

    Add files via upload

---
+++ DefiningClases/Exercises/Raw Data/Engine.java
@@ -0,0 +1,12 @@
+package Advanced.DefiningClasses.Exe.RawData;
+
+public class Engine {
+    private int speed;
+    private int power;
+
+    public Engine(int speed,int power){
+        this.speed=speed;
+        this.power=power;
+    }
+
+}
