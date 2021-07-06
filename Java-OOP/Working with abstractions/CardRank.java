8dbb7718ca1958a83fcdd793abc4096d4676663a
Petya Nacheva <79047302+PetyaNacheva@users.noreply.github.com>
2021-07-06 15:22:21 +0300

    Add files via upload

---
+++ Java-OOP/Working with abstractions/CardRank.java
@@ -0,0 +1,18 @@
+package OOP.Abstractions.Excercise.CardRank;
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+
+}
