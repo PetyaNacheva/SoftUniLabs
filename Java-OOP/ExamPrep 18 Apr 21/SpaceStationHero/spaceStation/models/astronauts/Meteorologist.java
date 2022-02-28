f6b89806736be7ef5cdedcd3a84c08b55f195a41
Petya Nacheva <79047302+PetyaNacheva@users.noreply.github.com>
2022-02-28 19:02:29 +0200

    Add files via upload

---
+++ Java-OOP/ExamPrep 18 Apr 21/SpaceStationHero/spaceStation/models/astronauts/Meteorologist.java
@@ -0,0 +1,32 @@
+package spaceStation.models.astronauts;
+
+public class Meteorologist extends BaseAstronaut{
+    public Meteorologist(String name) {
+        super(name, 90);
+    }
+
+    @Override
+    public void breath() {
+        super.breath();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Name: %s",getName()));
+        sb.append(System.lineSeparator());
+        sb.append(String.format("Oxygen: %.2f",getOxygen()));
+        sb.append(System.lineSeparator());
+        if(getBag().getItems().size()==0){
+            sb.append("none");
+        }else {
+            sb.append("Bag items: ");
+        }
+        for (String item : getBag().getItems()) {
+            sb.append(item+", ");
+        }
+        String result = sb.substring(0,sb.length()-2);
+
+        return result.trim();
+    }
+}
