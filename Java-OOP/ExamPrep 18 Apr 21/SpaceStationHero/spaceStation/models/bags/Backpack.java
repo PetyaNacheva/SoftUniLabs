c504106547d2ea539ba328f4d09f7ffc61aa730e
Petya Nacheva <79047302+PetyaNacheva@users.noreply.github.com>
2022-03-01 20:13:29 +0200

    Create Backpack.java

---
+++ Java-OOP/ExamPrep 18 Apr 21/SpaceStationHero/spaceStation/models/bags/Backpack.java
@@ -0,0 +1,22 @@
+package spaceStation.models.bags;
+
+import java.util.*;
+
+public class Backpack implements Bag{
+    private List<String> items;
+
+    public Backpack() {
+        this.items = new ArrayList<>();
+    }
+
+    @Override
+    public Collection<String> getItems() {
+        return this.items;
+    }
+
+    @Override
+    public void addItems(String item) {
+        items.add(item);
+    }
+
+}
